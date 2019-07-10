5d00ac1c045ae96c55920919179a535fb1a26602
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:33 -0300

    Arrais de duas

---
+++ ConsoleArrayBi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArrayBi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{ "Alice no país das maravilhas", "sim" },{"Pinóquio", "não"} };
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+
+            {
+                var Nomedolivro = listaDeLivros[i, 0];
+                var disponivel = listaDeLivros[i, 1];
+                Console.WriteLine($"Nome do livro:{Nomedolivro} disponivel: {disponivel}.");
+            }
+            Console.ReadKey();
+            }
+                }
+                    
+                
+    }
+
