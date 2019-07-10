4e2e676f4a067c790094eee3819496e78c03460f
John Doe <john.doe@gmail.com>
2019-07-10 10:22:53 -0300

    Mostrando a data e hora

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,8 +11,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git");
-            Console.ReadLine();
+            string[] nameList = new string[2] { "Valor 1", "Valor 2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyyMMddhmmssfff");
+                Console.WriteLine(dateList[i]);
+            }
+
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+
+            Console.ReadKey();
+            
+            
+            
+
+            
+            
         }
     }
 }
