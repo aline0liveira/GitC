fd103afbb10279221463d6fa3e9f3388cbaa7b1d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 17:27:04 -0300

    Select Aula 22/07

---
+++ AtividadeSelect/SQLQuery10.sql
@@ -0,0 +1,23 @@
+﻿select
+Carro,
+SUM(QuantidadeLocacao) as 'Total',
+MONTH([Data]) as 'Mês'
+From Locacoes
+
+Group by Carro, MONTH([Data])
+
+Order by Total asc
+
+
+
+select
+Carro,
+SUM(QuantidadeLocacao) as 'Total',
+year([Data]) as 'Ano'
+From Locacoes
+
+Group by Carro, year([Data])
+
+Order by Total asc
+
+
