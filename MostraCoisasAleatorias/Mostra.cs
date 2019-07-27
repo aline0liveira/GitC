09eef5a7bdbd432dcdbf88adc943f28041d7b5df
alinede0liveira <alinedeoliveira344@gmail.com>
2019-07-26 23:05:25 -0300

    Mostrando coisas aleatorias

---
+++ MostraCoisasAleatorias/Mostra.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostraCoisasAleatorias
+{
+    public class Mostra
+    {
+        public void MostraNome()
+        {
+            Console.WriteLine("Aline");
+        }
+
+        public void MostraDia( bool sextou)
+        {
+            
+            if (sextou)
+
+            Console.WriteLine("Hoje é sexta!!");
+
+            else
+
+             Console.WriteLine("Opps ainda é quinta");
+
+        }
+            public void MostraIdade(bool minhaIdade)
+            {
+                if(minhaIdade)
+                Console.WriteLine(" 18 ANOS");
+
+                else
+
+                Console.WriteLine("IDADE ERRADA");
+            }
+        
+    }
+}
