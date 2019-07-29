9b04aaeb0fd96e109ce4675a7bad94a80b4b09d7
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 13:26:58 -0300

    Exercicios encapsulamento

---
+++ SistemaCantina/Classes/Cantina.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCantina.Classes
+{
+
+    public class Cantina
+    {
+        public string Aluno { get; set; } = "Aline";
+
+        double saldo = 0;
+
+        
+
+        public double Saldo { get { return saldo; } }
+        public Cantina()
+        {
+            saldo = 20;
+
+        }
+
+        public bool Comprar(double valorLanche)
+        {
+            if(valorLanche <= saldo)
+            {
+                saldo -= valorLanche;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public double saldoAtual()
+        {
+            return saldo;
+        }
+       
+    }
+
+}
---
+++ SistemaCantina/Program.cs
@@ -0,0 +1,57 @@
+﻿using SistemaCantina.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCantina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cantina aluno = new Cantina();
+
+            Console.WriteLine($"Aluno: {aluno.Aluno}");
+
+            while (aluno.Saldo > 0.00)
+            {
+
+                Console.WriteLine("Menu - Lanches");
+                Console.WriteLine("1 - Pizaa 2,00.");
+                Console.WriteLine("2 - Hamburguer 5,00.");
+                Console.WriteLine("3 - HotDog  3,50.");
+                Console.WriteLine("Que lanche você deseja comprar?");
+
+
+
+                var escolha = Console.ReadLine();
+                var preco = 0.00;
+
+                switch (escolha)
+                {
+                    case "1": { preco = 2.00; } break;
+                    case "2": { preco = 5.00; } break;
+                    case "3": { preco = 3.50; } break;
+
+                }
+
+
+
+                if (preco > 0 && aluno.Comprar(preco))
+
+                    Console.WriteLine("Compra da merenda feita com sucesso!");
+
+                else
+                    Console.WriteLine("Vai ficar sem a merenda");
+
+
+                Console.WriteLine($"Atualmente seu saldo disponível pra merenda é: {aluno.Saldo.ToString("N2")}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
