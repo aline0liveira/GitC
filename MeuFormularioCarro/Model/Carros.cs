9bee6377ecf2b075bbd8dfc967bfdf39646c9668
alinede0liveira <alinedeoliveira344@gmail.com>
2019-07-30 21:14:47 -0300

    Exercico Carro

---
+++ MeuFormularioCarro/Model/Carros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuFormularioCarro.Model
+{
+   public class Carros
+    {
+
+        public string Modelo { get; set; }
+
+        public string Ano { get; set; }
+    }
+}
---
+++ MeuFormularioCarro/TelaCadastroCarro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MeuFormularioCarro.Model;
+
+namespace MeuFormularioCarro
+{
+    public partial class TelaCadastroCarro : Form
+    {
+        public TelaCadastroCarro()
+        {
+            InitializeComponent();
+        }
+
+       public Carros novoCarro = new Carros();
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            novoCarro.Modelo = txbModelo.Text;
+            novoCarro.Ano =  txbAno.Text;
+
+            this.Close();
+        }
+    }
+}
