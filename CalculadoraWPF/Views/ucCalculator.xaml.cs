8c063c4add652883b60da46194847273c0748273
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-07 09:54:17 -0300

    Exercicios

---
+++ CalculadoraWPF/Views/ucCalculator.xaml.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculadoraWPF.Views
+{
+    /// <summary>
+    /// Interação lógica para ucCalculator.xam
+    /// </summary>
+    public partial class ucCalculator : UserControl
+    {
+
+
+
+        double number1 = 0;
+        double number2 = 0;
+        string operation = "";
+
+        public ucCalculator()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 1;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 1;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 2;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 2;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 4;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 4;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 5;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 5;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 6;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 6;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 8;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 8;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 3;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 3;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 7;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 7;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 9;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) +9;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+
+            if (operation == "")
+            {
+                number1 = (number1 * 10) ;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) ;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void Button_Click_Plus(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            txtDisplay.Text = "0";
+        }
+
+        private void Button_Click_Subt(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+            txtDisplay.Text = "0";
+        }
+
+       
+
+        private void Button_Click_Multi(object sender, RoutedEventArgs e)
+        {
+            {
+                operation = "*";
+                txtDisplay.Text = "0";
+            }
+        }
+
+        private void Button_Click_Divide(object sender, RoutedEventArgs e)
+        {
+            {
+                operation = "/";
+                txtDisplay.Text = "0";
+            }
+        }
+
+        private void Button_Click_Equals(object sender, RoutedEventArgs e)
+        {
+            switch(operation )
+            {
+                case "+":
+                    txtDisplay.Text = (number1 + number2).ToString();
+                    break;
+
+                case "-":
+                    txtDisplay.Text = (number1 - number2).ToString();
+                    break;
+
+                case "*":
+                    txtDisplay.Text = (number1 * number2).ToString();
+                    break;
+
+                case "/":
+                    txtDisplay.Text = (number1 / number2).ToString();
+                    break;
+            }
+        }
+
+
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            if(operation == "")
+            {
+                number1 = 0;
+            }
+            else
+            {
+                number2 = 0;
+            }
+            txtDisplay.Text = "0";
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[,] livros = new string[2, 2] {
+            { " Alice no País das maravilhas", // Nome do Livro
+                "sim" }, // Disponível
+        { "Pinóquio", // Nome do Livro
+           "não" //Disponível     
+                } };
+
+        Console.ReadKey();
+            }
+}
+}
+
+
+
+
+#region Exercicio pronto 
+/* string[] stringListInfo = new string[2];
+ for (int i = 0; i < stringListInfo.Length; i++)
+ {
+
+     stringListInfo[i] =
+         $"Nome: {IsValid("", "Nome")} " +
+         $"Idade: {IsNumberFormat(IsValid("", "idade"), "00")} " +
+         $"Peso: {IsNumberFormat(IsValid("", "Peso"), "n2")} kg";
+
+ }
+
+ for (int i = 0; i < stringListInfo.Length; i++)
+     Console.WriteLine(stringListInfo[i]);
+ Console.ReadKey();
+}
+
+
+public static string IsValid(string value, string fieldName)
+{
+ var retorno = value;
+
+ while (string.IsNullOrEmpty(retorno) || string.IsNullOrWhiteSpace(retorno))
+ {
+     Console.Clear();
+     Console.WriteLine($"Informe um valor para o campo {fieldName}:");
+     retorno = Console.ReadLine();
+ }
+ return retorno;
+}
+private  static string IsNumberFormat(object values, string format)
+{
+ int.TryParse(values.ToString(), out int retorno);
+
+ return retorno.ToString(format);
+}
+}
+} */
+#endregion
+#region Comentarios
+//var count = 0;
+//while(count < 10)
+//{
+//    Console.WriteLine("Teste");
+//    count++;
+//
+//}
+//int[] teste = new int[5] { 0, 1, 2, 3, 4 };
+//for (int i=0; i< teste.Length; i++)
+//{
+//    teste[i] = i;
+//}
+//for (int i = 0; i < teste.Length; i++)
+//{
+//    Console.WriteLine(teste[i]);
+//}
+#endregion
+#region Exercício completo
+
+/*  string[] arrayStringContent = new string[3];
+
+
+for (int i = 0; i <arrayStringContent.Length; i++)
+{
+Console.WriteLine("Informe seu nome e idade:");
+arrayStringContent[i] = Console.ReadLine();
+Console.Clear();
+}
+for(int i = 0; i< arrayStringContent.Length; i++)
+{
+Console.WriteLine($"Nome e Idade: {arrayStringContent[i]}");
+}*/
+#endregion
+#region Exercicio modificado
+/*
+string[] listaDeNomes = new string[3];
+
+
+for (int i = 0; i < listaDeNomes.Length; i++)
+{
+ Console.WriteLine("Informe seu nome, idade e cor favorita:");
+ listaDeNomes[i] += Console.ReadLine();
+ Console.Clear();
+}
+for (int i = 0; i < listaDeNomes.Length; i++)
+{
+ Console.WriteLine($"Cadastrado: {listaDeNomes[i]}");
+}
+{
+
+ Console.WriteLine(" \n Cadastro Concluído com Sucesso!!!");
+}
+*/
+#endregion
