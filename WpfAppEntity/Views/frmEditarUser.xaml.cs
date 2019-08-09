6547c8ebaa776d2425c841db62374edd53ba6689
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-09 11:57:50 -0300

    Commit

---
+++ WpfAppEntity/Views/frmEditarUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppEntity.Data;
+
+namespace WpfAppEntity.Views
+{
+    /// <summary>
+    /// Lógica interna para frmEditarUser.xaml
+    /// </summary>
+    public partial class frmEditarUser : Window
+    {
+        public frmEditarUser()
+        {
+            InitializeComponent();
+        }
+        public Usuario user;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            user.Nome = tbxNome.Text;
+            user.Login = tbxLogin.Text;
+            user.Senha = tbxSenha.Text;
+
+            
+
+
+        }
+    }
+}
---
+++ WpfAppEntity/Views/ucGridView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppEntity.Data;
+
+namespace WpfAppEntity.Views
+{
+    /// <summary>
+    /// Interação lógica para ucGridView.xam
+    /// </summary>
+    public partial class ucGridView : UserControl
+    {
+        public ucGridView()
+        {
+            InitializeComponent();
+        }
+
+
+        public BibliotecaDbContext contex = new BibliotecaDbContext();
+
+        private void DtGridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            dtGridView.ItemsSource = contex.Usuarios.ToList<Usuario>();
+        }
+
+        private void DtGridView_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e) // quando terminei uma finalização do grid 
+        {
+            var objectToEdit = ((DataGrid)sender).SelectedValue as Usuario;
+
+
+
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+            contex.SaveChanges();
+
+            MessageBox.Show("Itens salvos com sucesso");
+        }
+    }
+}
